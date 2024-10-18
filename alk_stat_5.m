306a0f28642b9fb61b6a77f483855df810e73131
Ferenczi Benjámin <66739561+MrGreencock@users.noreply.github.com>
2024-10-18 09:09:43 +0200

    Update alk_stat_5.m

--- alk_stat_5.m
+++ alk_stat_5.m
@@ -4,5 +4,6 @@ int(x/(b-a),x,a,b);
 %normális eloszlás
 normcdf(-50,20, 50);
 normcdf(50,20, 50) - normcdf(-50,20, 50);
-1 - normcdf(50,20, 50)
+1 - normcdf(50,20, 50);
 
+nchoosek(10,2) * nchoosek()
